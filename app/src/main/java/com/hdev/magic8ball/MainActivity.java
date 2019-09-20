35ccc0ecb767bb315830567115bc03696870ad38
HeshanHH <heshanwijerthna01@gmail.com>
2019-09-20 21:01:31 +0530

    working fine.

---
+++ app/src/main/java/com/hdev/magic8ball/MainActivity.java
@@ -0,0 +1,44 @@
+package com.hdev.magic8ball;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ImageView ballDisplay = findViewById(R.id.image_eightBall);//value add
+        final int[] ballArray = new int[]{
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5};
+
+        Button muButton = findViewById(R.id.askButton);
+
+        muButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(5);
+                //int imageResourceId = ballArray[number];
+                ballDisplay.setImageResource(ballArray[number]);
+                // u can do it using oneLine ballDisplay.setImageResource(ballArray[number]);
+
+
+            }
+        });
+
+
+    }
+}
